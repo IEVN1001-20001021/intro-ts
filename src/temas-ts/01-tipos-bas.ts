15e2e9d4766bb4eeda2379cdc15e7691d2c44853
Fatima Alondra Reyes Lopez <reyeslopezal21@gmail.com>
2024-09-09 21:02:19 -0600

    Primer commit de intro-ts Lunes 9

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,27 @@
+
+//console.log('Hola Mundo')
+
+// Crear variables
+var nombre = 'Juan'
+let apellido = 'Perez'
+const edad = 30
+
+let num1 = 2
+let num2 = 'eyeyeyey'
+let num3 = 3
+
+let suma = num1 + num2 + num3
+console.log(suma)
+
+let correo:string;
+let edad2:number;
+let activo:boolean;
+
+correo = 'fatimaalondrar3@gmail.com'
+edad2 = 20
+activo = true
+
+let matricula:number|string|boolean;
+
+matricula=12345
+matricula='23AD45'
